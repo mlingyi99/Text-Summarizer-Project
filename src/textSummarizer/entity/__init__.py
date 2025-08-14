0de074e4a463f89b5f7c680745768fdf1039f1f5
Summer Meng <129761518+mlingyi99@users.noreply.github.com>
2025-08-14 12:50:33 -0400

    data ingestion notebook added

---
+++ src/textSummarizer/entity/__init__.py
@@ -0,0 +1,12 @@
+## Update entity
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+
+## Copy contents from config.yaml. This part is called entity 
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
